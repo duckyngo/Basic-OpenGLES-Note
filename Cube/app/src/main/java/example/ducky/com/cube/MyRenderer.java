b711553f1f1629626a7e05ccc001312020a616b7
duckyngo <duckyngo1705@gmail.com>
2017-03-18 17:56:42 +0700

    Update Cube

---
+++ Cube/app/src/main/java/example/ducky/com/cube/MyRenderer.java
@@ -0,0 +1,69 @@
+package example.ducky.com.cube;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by Ducky-PC on 3/18/2017.
+ */
+
+public class MyRenderer implements GLSurfaceView.Renderer {
+
+    private Cube mCube;
+
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+    private final float[] mRotationMatrix = new float[16];
+
+    private float mAngle;
+
+    public float getAngle() {
+        return mAngle;
+    }
+
+    public void setAngle(float mAngle) {
+        this.mAngle = mAngle;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+//        GLES20.glEnable(GLES20.GL_CULL_FACE);
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        mCube = new Cube();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+        GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width/ height;
+
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, 1, -1, 3, 100);
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+        float[] scrach = new float[16];
+
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        Matrix.setLookAtM(mViewMatrix, 0, -9, -9, -9, 0f, 0f, 0f, 0f, 1.0f, 0f);
+
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, 1.0f);
+
+        Matrix.multiplyMM(scrach, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+        mCube.draw(scrach);
+    }
+}
