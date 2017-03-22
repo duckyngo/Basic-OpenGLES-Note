336846e0995eb4710fc074814cfbdf3040665dc1
duckyngo <duckyngo1705@gmail.com>
2017-03-22 22:55:14 +0700

    Fix CubeTexture bug

--- CubeTexture/app/src/main/java/example/ducky/com/cubetexture/MainActivity.java
+++ CubeTexture/app/src/main/java/example/ducky/com/cubetexture/MainActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ToggleButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     FrameLayout glLayout;
     GLSurfaceView glSurfaceView;
     MyRenderer myRenderer;
+    private ToggleButton mXButton, mYButton, mZButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,44 @@ public class MainActivity extends Activity {
         glSurfaceView.setRenderer(myRenderer);
         glLayout.addView(glSurfaceView);
 
+        glSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float x = event.getX();
+                float y = event.getY();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+
+                        float dx = x - mPreviousX;
+                        float dy = y - mPreviousY;
+
+                        // reverse direction of rotation above the mid-line
+                        if (y > glSurfaceView.getHeight() / 2) {
+                            dx = dx * -1;
+                        }
+
+                        // reverse direction of rotation to left of the mid-line
+                        if (x < glSurfaceView.getWidth() / 2) {
+                            dy = dy * -1;
+                        }
+                        myRenderer.setAngle(
+                                myRenderer.getAngle() +
+                                        ((dx + dy) * TOUCH_SCALE_FACTOR));  // = 180.0f / 320
+
+                        glSurfaceView.requestRender();
+                }
+
+                mPreviousX = x;
+                mPreviousY = y;
+                return true;
+            }
+        });
+
+        mXButton = (ToggleButton) findViewById(R.id.Xaxis);
+        mYButton = (ToggleButton) findViewById(R.id.Yaxis);
+        mZButton = (ToggleButton) findViewById(R.id.Zaxis);
+
     }
 
     private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
@@ -30,38 +71,53 @@ public class MainActivity extends Activity {
     private float mPreviousY;
 
     @Override
-    public boolean onTouchEvent(MotionEvent e) {
-        // MotionEvent reports input details from the touch screen
-        // and other input controls. In this case, you are only
-        // interested in events where the touch position changed.
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Xaxis:
+                if (mXButton.isChecked()) {
+                    float temp[] =  {1.0f, 0.0f, 0.0f};
+                    myRenderer.setAngle(0);
+                    myRenderer.setmRotateCoordinate(temp);
 
-        float x = e.getX();
-        float y = e.getY();
+                    if (mYButton.isChecked())
+                        mYButton.setChecked(false);
 
-        switch (e.getAction()) {
-            case MotionEvent.ACTION_MOVE:
-
-                float dx = x - mPreviousX;
-                float dy = y - mPreviousY;
-
-                // reverse direction of rotation above the mid-line
-                if (y > glSurfaceView.getHeight() / 2) {
-                    dx = dx * -1 ;
+                    if (mZButton.isChecked())
+                        mZButton.setChecked(false);
                 }
+                break;
 
-                // reverse direction of rotation to left of the mid-line
-                if (x < glSurfaceView.getWidth() / 2) {
-                    dy = dy * -1 ;
+            case R.id.Yaxis:
+
+
+                if (mYButton.isChecked()) {
+                    float temp[] =  {0.0f, 1.0f, 0.0f};
+                    myRenderer.setAngle(0);
+                    myRenderer.setmRotateCoordinate(temp);
+
+                    if (mXButton.isChecked())
+                        mXButton.setChecked(false);
+
+                    if (mZButton.isChecked())
+                        mZButton.setChecked(false);
                 }
+                break;
 
-                myRenderer.setAngleZ(
-                        myRenderer.getAngleZ() +
-                                ((dx + dy) * TOUCH_SCALE_FACTOR));  // = 180.0f / 320
-                glSurfaceView.requestRender();
+            case R.id.Zaxis:
+
+
+                if (mZButton.isChecked()) {
+                    float temp[] =  {0.0f, 0.0f, 1.0f};
+                    myRenderer.setAngle(0);
+                    myRenderer.setmRotateCoordinate(temp);
+
+                    if (mYButton.isChecked())
+                        mYButton.setChecked(false);
+
+                    if (mXButton.isChecked())
+                        mXButton.setChecked(false);
+                }
+                break;
         }
-
-        mPreviousX = x;
-        mPreviousY = y;
-        return true;
     }
 }
--- CubeTexture/app/src/main/java/example/ducky/com/cubetexture/MyRenderer.java
+++ CubeTexture/app/src/main/java/example/ducky/com/cubetexture/MyRenderer.java
@@ -7,65 +7,97 @@ import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLUtils;
 import android.opengl.Matrix;
+import android.util.Log;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-/**
- * Created by Ducky-PC on 3/18/2017.
- */
-
 public class MyRenderer implements GLSurfaceView.Renderer {
 
+    private static final String TAG = "ky.nd";
     private Cube mCube;
     private Context mContext;
-    private final float[] mMVPMatrix = new float[16];
-    private final float[] mProjectionMatrix = new float[16];
-    private final float[] mViewMatrix = new float[16];
-    private final float[] mRoationMatrix = new float[16];
-
-    private float mAngleZ, mAngleX, mAngleY;
 
     public MyRenderer(Context mContext) {
         this.mContext = mContext;
     }
 
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+    private final float[] mRoationMatrix = new float[16];
+
+    private float mAngle;
+    private float mRotateCoordinate[] = {0.0f, 0.0f, 1.0f};
+
+    public static int loadShader(int type, String shaderCode){
+
+        int shader = GLES20.glCreateShader(type);
+
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
 
         mCube = new Cube(mContext);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-
         GLES20.glViewport(0, 0, width, height);
 
-        float ratio = (float) width/ height;
+        float ratio = (float) width / height;
 
-        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, 1, -1, 3, 7);
-
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 1, 100);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-
         float[] scratch = new float[16];
 
+        // Draw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -7, 0f, 0f, 0f, 0f, 1.0f, 0f);
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, -3, -3, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
 
+        // Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
-
-        Matrix.setRotateM(mRoationMatrix, 0, mAngleZ, 1.0f, 0, 0.0f);
+        Matrix.setRotateM(mRoationMatrix, 0, mAngle, mRotateCoordinate[0], mRotateCoordinate[1], mRotateCoordinate[2]);
 
 
         Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRoationMatrix, 0);
 
-        mCube.draw(scratch);
+        // Draw triangle
+        mCube.draw(scratch, mMVPMatrix);
+    }
+
+
+    public void setmRotateCoordinate(float[] mRotateCoordinate) {
+        this.mRotateCoordinate = mRotateCoordinate;
+    }
+
+    public float getAngle() {
+        return mAngle;
+    }
+
+    public void setAngle(float mAngle) {
+        this.mAngle = mAngle;
+    }
+
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
     }
 
     public static int loadTexture(final Context context, final  int resID){
@@ -103,27 +135,4 @@ public class MyRenderer implements GLSurfaceView.Renderer {
         return textureHandle[0];
     }
 
-    public float getAngleZ() {
-        return mAngleZ;
-    }
-
-    public void setAngleZ(float mAngleZ) {
-        this.mAngleZ = mAngleZ;
-    }
-
-    public float getAngleX() {
-        return mAngleX;
-    }
-
-    public void setAngleX(float mAngleX) {
-        this.mAngleX = mAngleX;
-    }
-
-    public float getAngleY() {
-        return mAngleY;
-    }
-
-    public void setAngleY(float mAngleY) {
-        this.mAngleY = mAngleY;
-    }
 }
